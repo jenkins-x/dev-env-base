cae7e0a3ac1ef787fde54f65442341520e60448d
macox <markcox20@hotmail.com>
2019-07-03 14:46:18 +0100

    Auto update dev-env with base version
    Signed-off-by: macox <markcox20@hotmail.com>

---
+++ scripts/update-bot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+VERSION=$1
+./build/linux/jx step create pr regex --regex "DEV_ENV_BASE_VERSION=(.*)" --version $VERSION --files dev-env --repo git@github.com:jenkins-x/dev-env.git
